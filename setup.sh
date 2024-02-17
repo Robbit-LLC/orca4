b89feb6ea95e8d894d926aadf2f92f991d3bb477
ocadovski <ognjen.cadovski@robbit.rs>
2024-02-17 18:31:57 +0100

    Fix setup script
    Rename setup.bash to setup.sh (to be universal), and replace hardcoded path with relative paths (assuming that 'ardupilot' and 'ardupilot_gazebo' directories are located in parent folder).

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Modify this for your environment
+
+export PROJECT_PATH=$(pwd)/..
+
+# Add results of ArduSub build
+export PATH=$PROJECT_PATH/ardupilot/build/sitl/bin:$PATH
+
+# Optional: add autotest to the PATH, helpful for running sim_vehicle.py
+export PATH=$PROJECT_PATH/ardupilot/Tools/autotest:$PATH
+
+# Add results of colcon build
+source $PROJECT_PATH/colcon_ws/install/setup.sh
+
+# Add ardupilot_gazebo plugin
+export GZ_SIM_SYSTEM_PLUGIN_PATH=$PROJECT_PATH/ardupilot_gazebo/build:$Z_SIM_SYSTEM_PLUGIN_PATH
+
+# Optional: add ardupilot_gazebo models and worlds
+export GZ_SIM_RESOURCE_PATH=$PROJECT_PATH/ardupilot_gazebo/models:$PROJECT_PATH/ardupilot_gazebo/worlds:$GZ_SIM_RESOURCE_PATH
+
+# Add bluerov2_ignition models and worlds
+export GZ_SIM_RESOURCE_PATH=$PROJECT_PATH/colcon_ws/src/bluerov2_ignition/models:$PROJECT_PATH/colcon_ws/src/bluerov2_ignition/worlds:$GZ_SIM_RESOURCE_PATH
+
+# Add orca4 models and worlds
+export GZ_SIM_RESOURCE_PATH=$PROJECT_PATH/colcon_ws/src/orca4/orca_description/models:$PROJECT_PATH/colcon_ws/src/orca4/orca_description/worlds:$GZ_SIM_RESOURCE_PATH
+
+# Build ros_gz on the humble branch for Gazebo Garden
+export GZ_VERSION=garden
